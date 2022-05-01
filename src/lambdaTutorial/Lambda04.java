050afbd00bfc1a2953407757b01bd2785f95771d
berkc <b.cal.2193@gmail.com>
2022-05-01 17:39:43 +0200

    lambda

--- src/lambdaTutorial/Lambda04.java
+++ src/lambdaTutorial/Lambda04.java
@@ -31,7 +31,43 @@ public class Lambda04 {
         System.out.println(bölümKontrol(ünv));
         System.out.println("\n*********************");
 
-        ögrenciSayisiSirala(ünv);
+        System.out.println(ögrenciSayisiSirala(ünv));
+        System.out.println("\n*********************");
+
+        ögrenciSayisiVoid(ünv);
+        System.out.println("\n*********************");
+
+        notOrtalamasiSirala(ünv);
+        System.out.println("\n*********************");
+
+        ögrenciSayisienAzIkinci(ünv);
+        System.out.println("\n*********************");
+
+        System.out.println(enAzOgrcSaysi2Unv(ünv));
+        System.out.println("\n*********************");
+
+        notOrtalamasi63tenBüyüklerinÖgrenciSayisi(ünv);
+        System.out.println("\n*********************");
+
+        System.out.println(notOrtalamasi63tenBüyüklerinÖgrenciSayisi2(ünv));
+        System.out.println("\n*********************");
+
+        System.out.println(notOrtalamasi63tenBüyüklerinÖgrenciSayisiToInt(ünv));
+        System.out.println("\n*********************");
+
+        System.out.println(notOrtalamasi130danBüyüklerinNotOrtalamasi(ünv));
+        System.out.println("\n*********************");
+
+        System.out.println(matematikBölümleriSayisi(ünv));
+        System.out.println("\n*********************");
+
+        System.out.println(ögrenciSayilari130danBüyüklerinEnBüyükNotOrtalamasi(ünv));
+        System.out.println("\n*********************");
+
+        System.out.println(ögrenciSayilari130danBüyüklerinEnBüyükNotOrtalamasi2(ünv));
+        System.out.println("\n*********************");
+
+        System.out.println(ögrenciSayilari150denAzEnKücükNotOrtalamasi(ünv));
         System.out.println("\n*********************");
 
 
@@ -55,52 +91,94 @@ public class Lambda04 {
 
 
     //task 04-->universite'leri ogr sayilarina gore b->k siralayiniz.
-    public static void ögrenciSayisiSirala(List<Üniversite> ünv) {
+    public static List<Üniversite> ögrenciSayisiSirala(List<Üniversite> ünv) {
+        return ünv.
+                stream().
+                sorted(Comparator.comparing(Üniversite::getÖgrcSayisi).reversed()).
+                collect(Collectors.toList());
+        /*
+        collect() akistaki elemanlari istenen sarta göre toplar
+         */
+    }
 
+    public static void ögrenciSayisiVoid(List<Üniversite> ünv) {
+        System.out.println(ünv.
+                stream().
+                sorted(Comparator.comparing(Üniversite::getÖgrcSayisi).reversed()).
+                collect(Collectors.toList()));
     }
 
 
     //task 05-->universite'leri notOrt gore  b->k siralayip ilk 3 'unu print ediniz.
     public static void notOrtalamasiSirala(List<Üniversite> ünv) {
-
+        System.out.println(ünv.stream().sorted(Comparator.comparing(Üniversite::getNotOrt).reversed()).limit(3).collect(Collectors.toList()));
     }
 
 
     //task 06--> ogrc sayisi en az olan 2. universite'yi  print ediniz.
     public static void ögrenciSayisienAzIkinci(List<Üniversite> ünv) {
+        System.out.println(ünv.stream().sorted(Comparator.comparing(Üniversite::getÖgrcSayisi)).skip(1).findFirst());
+
+        System.out.println(ünv.stream().sorted(Comparator.comparing(Üniversite::getÖgrcSayisi)).skip(1).iterator().next());
 
     }
 
+    //task 06--> ogrc sayisi en az olan 2. universite'yi  print ediniz.
+    public static List<Üniversite> enAzOgrcSaysi2Unv(List<Üniversite> unv) {
+        return unv.
+                stream().
+                sorted(Comparator.comparing(Üniversite::getÖgrcSayisi)).
+                limit(2).
+                skip(1).
+                collect(Collectors.toList());
+
+    }
 
     //task 07--> notOrt 63 'den buyuk olan universite'lerin ogrc sayilarini toplamini print ediniz
     public static void notOrtalamasi63tenBüyüklerinÖgrenciSayisi(List<Üniversite> ünv) {
-
+        int toplam = ünv.stream().filter(t -> t.getNotOrt() > 63).map(t -> t.getÖgrcSayisi()).reduce(0, (a, b) -> a + b);
+        System.out.println(toplam);
     }
 
+    public static int notOrtalamasi63tenBüyüklerinÖgrenciSayisi2(List<Üniversite> ünv) {
+        return ünv.stream().filter(t -> t.getNotOrt() > 63).map(t -> t.getÖgrcSayisi()).reduce(0, (a, b) -> a + b);
+    }
 
+    public static int notOrtalamasi63tenBüyüklerinÖgrenciSayisiToInt(List<Üniversite> ünv) {
+        return ünv.stream().
+                filter(t -> t.getNotOrt() > 63).
+                mapToInt(t -> t.getÖgrcSayisi()). //mapToInt methodu hepsini inte cevirecegi icin ekstra reduce(toplama) islemine gerek yoktur
+                //sonuna .sum() methodu kullanarak int degerlerin toplamini alabiliriz
+                        sum();
+        //mapToInt() --> akistaki elemanlarin data typeini parametresindeki() degere göre Int update eder
 
+    }
 
     //task 08--> Ogrenci sayisi 130'dan buyuk olan universite'lerin notOrt'larinin ortalamasini bulunuz.
-    public static void notOrtalamasi130danBüyüklerinNotOrtalamasi(List<Üniversite> ünv) {
-
+    public static OptionalDouble notOrtalamasi130danBüyüklerinNotOrtalamasi(List<Üniversite> ünv) {
+        return ünv.stream().filter(t -> t.getÖgrcSayisi() > 130).mapToDouble(t -> t.getNotOrt()).average();
+        //mapToDouble() --> akistaki elemanlarin data typeini parametresindeki() degere göre double update eder
     }
-
 
     //task 09-->"matematik" bolumlerinin sayisini  print ediniz.
-    public static void matematikBölümleriSayisi(List<Üniversite> ünv) {
-
+    public static int matematikBölümleriSayisi(List<Üniversite> ünv) {
+        return (int) ünv.stream().filter(t -> t.getBölüm().equalsIgnoreCase("matematik")).count();
     }
 
 
-    //task 10-->Ogrenci sayilari 130'dan fazla olan universite'lerin en buyuk notOrt'unu bulunuz
-    public static void ögrenciSayilari130danBüyüklerinEnBüyükNotOrtalamasi(List<Üniversite> ünv) {
+    //task 10-->Ogrenci sayilari 571'dan fazla olan universite'lerin en buyuk notOrt'unu bulunuz
+    public static Optional<Üniversite> ögrenciSayilari130danBüyüklerinEnBüyükNotOrtalamasi(List<Üniversite> ünv) {
+        return ünv.stream().filter(t -> t.getÖgrcSayisi() > 571).sorted(Comparator.comparing(Üniversite::getNotOrt).reversed()).findFirst();
+    }
 
+    public static OptionalInt ögrenciSayilari130danBüyüklerinEnBüyükNotOrtalamasi2(List<Üniversite> ünv) {
+        return ünv.stream().filter(t -> t.getÖgrcSayisi() > 571).mapToInt(t -> t.getNotOrt()).max();
     }
 
 
-    //task 11-->Ogrenci sayilari 150'dan az olan universite'lerin en kucuk notOrt'unu bulunuz.
-    public static void ögrenciSayilari150denAzEnKücükNotOrtalamasi(List<Üniversite> ünv) {
-
+    //task 11-->Ogrenci sayilari 1071'dan az olan universite'lerin en kucuk notOrt'unu bulunuz.
+    public static OptionalInt ögrenciSayilari150denAzEnKücükNotOrtalamasi(List<Üniversite> ünv) {
+        return ünv.stream().filter(t -> t.getÖgrcSayisi() < 1071).mapToInt(t -> t.getNotOrt()).min();
     }
 
 
